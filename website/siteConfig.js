7026a9665f2d1c8fc0008f9c0776e3f5ece31f5e
Oleg Sorokotyaga <ext.osorokotyaga@tcsbank.ru>
2018-11-02 10:22:22 +0300

    update website/siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,14 +13,14 @@
 const siteConfig = {
     title: '@tinkoff/request' /* title for your website */,
     tagline: 'Construct your own request library',
-    baseUrl: '/request/' /* base url for your project */,
+    baseUrl: '/tinkoff-request/' /* base url for your project */,
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
-    projectName: '@tinkoff/request',
-    organizationName: 'Tinkoff',
+    projectName: 'tinkoff-request',
+    organizationName: 'TinkoffCreditSystems',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
