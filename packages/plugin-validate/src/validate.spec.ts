e01772bdc31cc5a3f6b9b5c7d90a4a16d257a6cf
osorokotyaga <35488154+osorokotyaga@users.noreply.github.com>
2019-02-12 10:07:47 +0300

    feat(plugin-validate): add errorValidator option (#6)

--- packages/plugin-validate/src/validate.spec.ts
+++ packages/plugin-validate/src/validate.spec.ts
@@ -1,4 +1,5 @@
 import { Context, Status } from '@tinkoff/request-core';
+import { VALIDATE } from './constants';
 import validate from './validate';
 
 const validator = ({ response }) => {
@@ -6,12 +7,25 @@ const validator = ({ response }) => {
         return new Error(response.error);
     }
 };
-const plugin = validate({ validator });
+
+const errorValidator = ({ error }) => {
+    return !!error.valid;
+};
+
+const plugin = validate({ validator, errorValidator });
 const context = new Context();
 
 context.setState = jest.fn(context.setState.bind(context));
+context.updateMeta = jest.fn(context.updateMeta.bind(context));
 
 describe('plugins/validate/validate', () => {
+    beforeEach(() => {
+        // @ts-ignore
+        context.setState.mockClear();
+        // @ts-ignore
+        context.updateMeta.mockClear();
+    });
+
     it('if validator returns undefined plugin should not return any state or call next callback', () => {
         context.setState({ response: { a: 1 } });
         (context.setState as jest.Mock).mockClear();
@@ -19,6 +33,7 @@ describe('plugins/validate/validate', () => {
 
         expect(plugin.complete(context, next, null)).toBeUndefined();
         expect(context.setState).not.toHaveBeenCalled();
+        expect(context.updateMeta).toHaveBeenCalledWith(VALIDATE, { validated: true });
         expect(next).toHaveBeenCalledWith();
     });
 
@@ -48,6 +63,26 @@ describe('plugins/validate/validate', () => {
             request: { ...request, fallbackCache: false },
             status: Status.ERROR,
             error: new Error('123')
+        });
+    });
+
+    it('on errored request, errorValidator may change request to complete', () => {
+        const response = { test: 'abc' };
+        const error = Object.assign(new Error(), { valid: true });
+        const next = jest.fn();
+
+        context.setState({ error, response });
+
+        plugin.error(context, next, null);
+        expect(next).toHaveBeenCalledWith({
+            status: Status.COMPLETE,
+            error: null,
+        });
+        expect(context.updateMeta).toHaveBeenCalledWith(VALIDATE, {
+            errorValidated: true,
+        });
+        expect(context.updateMeta).toHaveBeenCalledWith(VALIDATE, {
+            error,
         });
     });
 });
