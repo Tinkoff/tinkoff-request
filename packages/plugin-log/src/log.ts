d10643545ccb0a98361b30bf89aade138cebc40e
Kirill Meleshko <8974488+meskill@users.noreply.github.com>
2020-03-27 16:46:32 +0300

    feat(log): mask query and payload values by default
    Ability to show these values are controlled by options showQueryFields and showPayloadFields

--- packages/plugin-log/src/log.ts
+++ packages/plugin-log/src/log.ts
@@ -1,7 +1,17 @@
 import prop from '@tinkoff/utils/object/prop';
-import pick from '@tinkoff/utils/object/pick';
+import mapObj from '@tinkoff/utils/object/map';
+import isArray from '@tinkoff/utils/is/array';
+import isObject from '@tinkoff/utils/is/object';
 import { Context, Plugin } from '@tinkoff/request-core';
 import { LOG } from './constants/metaTypes';
+
+declare module '@tinkoff/request-core/lib/types.h' {
+    export interface Request {
+        silent?: boolean;
+        showQueryFields?: boolean | string[];
+        showPayloadFields?: boolean | string[];
+    }
+}
 
 const fillDuration = (context: Context) => {
     const meta = context.getExternalMeta(LOG);
@@ -30,13 +40,29 @@ const defaultLogger = (name: string): Logger => {
     };
 };
 
-const getInfo = pick(['url', 'query', 'payload']);
+const hideValues = (obj: any, showFields: Set<string> | boolean) => {
+    if (showFields === true) {
+        return obj;
+    }
+
+    if (!isObject(obj)) {
+        return '*';
+    }
+
+    if (showFields === false) {
+        return mapObj(() => '*', obj);
+    }
+
+    return mapObj((v, k) => (showFields.has(k) ? v : '*'), obj);
+};
 
 /**
  * Logs request events and timing
  *
  * requestParams:
  *      silent {boolean}
+ *      showQueryFields {boolean | string[]} - whether the plugin should show request query values
+ *      showPayloadFields {boolean | string[]} - whether the plugin should show request payload values
  *
  * metaInfo:
  *      log.start {number} - request start Date.now()
@@ -45,10 +71,44 @@ const getInfo = pick(['url', 'query', 'payload']);
  *
  * @param name {string}
  * @param logger {Function} - logger factory
+ * @param showQueryFields {boolean | string[]} - whether the plugin should show request query values
+ * @param showPayloadFields {boolean | string[]} - whether the plugin should show request payload values
  * @return {{init: init, complete: complete, error: error}}
  */
-export default ({ name = '', logger = defaultLogger }): Plugin => {
+export default ({
+    name = '',
+    logger = defaultLogger,
+    showQueryFields: globalShowQueryFields = false,
+    showPayloadFields: globalShowPayloadFields = false,
+}: {
+    name?: string;
+    logger?: (name: string) => Logger;
+    showQueryFields?: boolean | string[];
+    showPayloadFields?: boolean | string[];
+}): Plugin => {
     const log = logger(`request.${name}`);
+    const getInfo = ({
+        url,
+        query,
+        payload,
+        showQueryFields,
+        showPayloadFields,
+    }: {
+        url: string;
+        query?: Record<string, string>;
+        payload?: any;
+        showQueryFields?: boolean | string[];
+        showPayloadFields?: boolean | string[];
+    }) => {
+        const showQuery = showQueryFields ?? globalShowQueryFields;
+        const showPayload = showPayloadFields ?? globalShowPayloadFields;
+
+        return {
+            url,
+            query: query && hideValues(query, isArray(showQuery) ? new Set(showQuery) : showQuery),
+            payload: payload && hideValues(payload, isArray(showPayload) ? new Set(showPayload) : showPayload),
+        };
+    };
 
     return {
         init: (context, next) => {
